9eabf1652550d4ff18b65994a2b2703f47a1833d
duceux <duceux.guillaume@gmail.com>
2016-01-13 00:56:52 +0100

    removed stupid prints, will be replaced by a smart logging system

--- arty/arty.py
+++ arty/arty.py
@@ -7,19 +7,15 @@ if __name__ == "__main__":
     print("Hello, How can I help you?")
     while 1:
         user_input = input();
-        print("U: " + user_input)
         
         try:
-            print("Trying python engine")
             eval_res = eval(user_input)
-            print("R: " + str(eval_res))
+            print(eval_res)
             continue
         except Exception as e:
-            print(e)
-            print("E: python engine failed")
+            pass
 
         try:
-            print("Trying system engine")
             with subprocess.Popen(user_input, stdout=subprocess.PIPE, bufsize=1, 
                                   universal_newlines=True, shell=True) as child:
                 for line in child.stdout:
@@ -29,8 +25,6 @@ if __name__ == "__main__":
                 if rc == 0:
                     continue
                 else:
-                    print("E: system engine failed")
+                    pass
         except Exception as e:
-            print(e)
-            print("E: system engine failed")
-
+            pass
