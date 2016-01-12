b4a4532235a15ee354549e34567582f3e03e87c5
duceux <duceux.guillaume@gmail.com>
2016-01-12 18:08:09 +0100

    system engine embryo

--- arty.py
+++ arty.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
+import subprocess
+
 if __name__ == "__main__":
     print("Hello, How can I help you?")
     while 1:
         user_input = input();
         print("U: " + user_input)
-        # Handle python stuff
+        
         try:
+            print("Trying python engine")
             eval_res = eval(user_input)
             print("R: " + str(eval_res))
-        except:
-            print("R: not python")
-            pass
+            continue
+        except Exception as e:
+            print(e)
+            print("E: python engine failed")
+
+        try:
+            print("Trying system engine")
+            stdoutdata = subprocess.getoutput(user_input)
+            print("R: " + stdoutdata)
+        except Exception as e:
+            print(e)
+            print("E: system engine failed")
+
