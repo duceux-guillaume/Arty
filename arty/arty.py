f77ab1d10c9efd009ade0149daa51c246ca65732
duceux <duceux.guillaume@gmail.com>
2016-01-13 10:02:13 +0100

    remove use of shell

--- arty/arty.py
+++ arty/arty.py
@@ -3,7 +3,7 @@
 import subprocess
 import readline
 
-Commands = {'git log': ('shell', 'git log | more'),
+Commands = {'git log': ('shell', 'git log'),
             'what is your name?': ('chatter', 'Arty')}
 
 class PythonEngine:
@@ -18,14 +18,16 @@ class PythonEngine:
 class ShellEngine:
     def execute(self, cmd):
         try:
-            with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, 
-                                  universal_newlines=True, shell=True) as child:
+            with subprocess.Popen(cmd.split(' '), 
+                                  stdout=subprocess.PIPE, 
+                                  bufsize=1, 
+                                  universal_newlines=True) as child:
                 for line in child.stdout:
                     print(line, end='')
-                #output = child.communicate()
-                #rc = child.returncode
+                output = child.communicate()
+                rc = child.returncode
             return True
-        except:
+        except Exception as e:
             return False
 
 class ChatterEngine:
@@ -42,10 +44,13 @@ if __name__ == "__main__":
     print("Hello, How can I help you?")
     
     while 1:
-        user_input = input();
+        user_input = input("> ");
        
         # Command lookup
         try:
+            if len(user_input) == 0:
+                chatter.execute(user_input)
+                continue
             engine, cmd = Commands[user_input]
             if engine == 'shell':
                 shell.execute(cmd) 
