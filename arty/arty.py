19e7889f0809f1ad8591cb207569ba50c703811d
duceux <duceux.guillaume@gmail.com>
2016-01-13 01:57:30 +0100

    engine classes, basic cmd lookup, concept of chatter engine

--- arty/arty.py
+++ arty/arty.py
@@ -3,28 +3,62 @@
 import subprocess
 import readline
 
-if __name__ == "__main__":
-    print("Hello, How can I help you?")
-    while 1:
-        user_input = input();
-        
-        try:
-            eval_res = eval(user_input)
-            print(eval_res)
-            continue
-        except Exception as e:
-            pass
+Commands = {'git log': ('shell', 'git log | more'),
+            'what is your name?': ('chatter', 'Arty')}
 
+class PythonEngine:
+    def execute(self, cmd):
         try:
-            with subprocess.Popen(user_input, stdout=subprocess.PIPE, bufsize=1, 
+            eval_res = eval(cmd)
+            print(eval_res)
+            return True
+        except:
+            return False
+
+class ShellEngine:
+    def execute(self, cmd):
+        try:
+            with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, 
                                   universal_newlines=True, shell=True) as child:
                 for line in child.stdout:
                     print(line, end='')
-                output = child.communicate()
-                rc = child.returncode
-                if rc == 0:
-                    continue
-                else:
-                    pass
+                #output = child.communicate()
+                #rc = child.returncode
+            return True
+        except:
+            return False
+
+class ChatterEngine:
+    def execute(self, cmd):
+        print(cmd)
+        return True
+
+if __name__ == "__main__":
+
+    python = PythonEngine()
+    shell = ShellEngine()
+    chatter = ChatterEngine()
+
+    print("Hello, How can I help you?")
+    
+    while 1:
+        user_input = input();
+       
+        # Command lookup
+        try:
+            engine, cmd = Commands[user_input]
+            if engine == 'shell':
+                shell.execute(cmd) 
+            elif engine == 'python':
+                python.execute(cmd)
+            elif engine == 'chatter':
+                chatter.execute(cmd)
         except Exception as e:
-            pass
+            # No match, try every engine
+            if python.execute(user_input):
+                continue
+            if shell.execute(user_input):
+                continue
+            if chatter.execute("wtf?"):
+                continue
+
