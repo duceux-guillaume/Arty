2ebcc98a0f227d1027323bb1b81a72c28606731c
duceux <duceux.guillaume@gmail.com>
2016-01-17 03:17:56 +0100

    implement cd, correct call to fonction so that more, ls, vim etc... works

--- arty/arty.py
+++ arty/arty.py
@@ -2,8 +2,11 @@
 
 import subprocess
 import readline
+import engine
+import string_matching
 
-Commands = {'git log': ('shell', 'git log'),
+Commands = {'ls': ('shell', 'ls --color=auto'),
+            'll': ('shell', 'ls -la --color=auto'),
             'what is your name?': ('chatter', 'Arty')}
 
 class PythonEngine:
@@ -15,21 +18,6 @@ class PythonEngine:
         except:
             return False
 
-class ShellEngine:
-    def execute(self, cmd):
-        try:
-            with subprocess.Popen(cmd.split(' '), 
-                                  stdout=subprocess.PIPE, 
-                                  bufsize=1, 
-                                  universal_newlines=True) as child:
-                for line in child.stdout:
-                    print(line, end='')
-                output = child.communicate()
-                rc = child.returncode
-            return True
-        except Exception as e:
-            return False
-
 class ChatterEngine:
     def execute(self, cmd):
         print(cmd)
@@ -38,7 +26,7 @@ class ChatterEngine:
 if __name__ == "__main__":
 
     python = PythonEngine()
-    shell = ShellEngine()
+    shell = engine.ShellEngine()
     chatter = ChatterEngine()
 
     print("Hello, How can I help you?")
@@ -52,12 +40,20 @@ if __name__ == "__main__":
                 chatter.execute(user_input)
                 continue
             engine, cmd = Commands[user_input]
+            returncode = False
             if engine == 'shell':
-                shell.execute(cmd) 
+                returncode = shell.execute(cmd) 
             elif engine == 'python':
-                python.execute(cmd)
+                returncode = python.execute(cmd)
             elif engine == 'chatter':
-                chatter.execute(cmd)
+                returncode = chatter.execute(cmd)
+            if not returncode: # Try all
+                if python.execute(user_input):
+                    continue
+                if shell.execute(user_input):
+                    continue
+                if chatter.execute("wtf?"):
+                    continue
         except Exception as e:
             # No match, try every engine
             if python.execute(user_input):
