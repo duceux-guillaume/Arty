20fbbadc88366ce4d0db699021bcd289260fd348
Guillaume Duceux <duceux.guillaume@gmail.com>
2019-06-18 21:06:13 +0200

    move things around

--- src/bin/calc.cpp
+++ src/bin/calc.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <limits>
 
 #include <arty/math/core/number.h>
 
 using namespace arty::math;
 
 int main(int argc, char** argv) {
-    std::cout << argc << std::endl;
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << std::endl;
+    std::string input;
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            input.append(argv[i]);
+        }
     }
 
-    std::cout << Number(1568792) << std::endl;
-
-    std::cout << Number(10) + Number(850) << std::endl;
-    std::cout << std::numeric_limits<int>::max() << std::endl;
-    std::cout << Number(std::numeric_limits<int>::max()) + std::numeric_limits<int>::max() << std::endl;
-    std::cout << std::numeric_limits<long>::max() << std::endl;
-    std::cout << Number(std::numeric_limits<long>::max()) + std::numeric_limits<long>::max() << std::endl;
-
-    Number A(1);
-    Number B(2);
-    std::cout << A << " - " << B << " = " << A - B << std::endl;
-
-    std::cout << Number(-123456) << std::endl;
-    std::cout << Number(123456) << std::endl;
-
-    std::cout << Number(-5000, 100) << std::endl;
-    std::cout << Number(-1, 1) << std::endl;
+    std::cout << input << std::endl;
 
     return 0;
 }
